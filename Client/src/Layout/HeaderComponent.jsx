503d605b429aba3730559ce05228828053b081c9
Aaron R. Wilson <visionsic@student.fullsail.edu>
2021-02-09 15:47:23 -0500

    Optimizing folder structure  for GoWilder-React App.

---
+++ Client/src/Layout/HeaderComponent.jsx
@@ -0,0 +1,26 @@
+/*
+; =======================================================
+; Title: HeaderComponent.js - GoWilderApp0 - layout folder 
+; Authors: Aaron Wilson
+; Instructor: Sean Bernath
+; Date: 08 February 2021
+; Description: Build out app's Header component.
+; Legend: [ Aaron Wilson ] -> Team member responsible
+; for page.
+; =======================================================
+*/
+
+import React from 'react';
+
+const HeaderComponent = (props) => {
+
+    return (
+      <>
+        <header className="App-header">
+          Test API Call
+        </header>
+      </>
+    )
+  }
+
+export default  HeaderComponent;
---
+++ Client/src/index.jsx
@@ -0,0 +1,30 @@
+/*
+; =======================================================
+; Title: index.js - GoWilderApp0 - Root folder 
+; Authors: Aaron Wilson
+; Instructor: Sean Bernath
+; Date: 08 February 2021
+; Description: Build out app's index jsx file.
+; Legend: [ Aaron Wilson ] -> Team member responsible
+; for page.
+; =======================================================
+*/
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
